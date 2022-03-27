3d15514c74905d935d661041c548ea18bb9f190d
Moussa Saidi <90933411+ElevenMou@users.noreply.github.com>
2022-03-27 15:36:21 +0000

    profile edit

---
+++ app/Http/Livewire/ProfileEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ProfileEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile-edit');
+    }
+}
